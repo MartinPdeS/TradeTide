20aae0cdaa53e7b13b88e52ca55d0a8e0ce6779c
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-02-20 16:24:09 -0500

    adding holding shading for plottings

--- TradeTide/plottings.py
+++ TradeTide/plottings.py
@@ -2,28 +2,26 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 
-def plot_trading_strategy(market, portfolio, strategy, title='Trading Strategy Overview'):
-    """
-    Plots the trading strategy performance including price, buy/sell signals,
-    and other relevant indicators like moving averages or RSI.
+def plot_metrics_on_ax(ax, market, portfolio, strategy):
+    strategy.add_to_ax(ax)
 
-    Parameters:
-    - data: DataFrame containing the historical data and trading indicators/signals.
-    - title: str, optional. The title of the plot.
-    """
-    fig, axs = plt.subplots(2, 1, figsize=(7, 5), gridspec_kw={'height_ratios': [3, 1]}, sharex=True)
-    fig.suptitle(title)
-    axs[0].grid(False)
+    # Customize the indicators plot
+    ax.legend()
+    ax.grid(True)
+
+
+def plot_price_and_signal(ax, market, portfolio, strategy):
+    ax.grid(False)
 
     # Price and signals plot
-    axs[0].plot(
+    ax.plot(
         market.date,
         market['close'],
         label='Close Price',
         color='skyblue'
     )
 
-    axs[0].scatter(
+    ax.scatter(
         portfolio.date[portfolio['opened_positions'] == 1],
         market['close'][portfolio['opened_positions'] == 1],
         label='Buy Signal',
@@ -34,7 +32,7 @@ def plot_trading_strategy(market, portfolio, strategy, title='Trading Strategy O
         zorder=-1
     )
 
-    axs[0].scatter(
+    ax.scatter(
         portfolio.date[portfolio['opened_positions'] == -1],
         market['close'][portfolio['opened_positions'] == -1],
         label='Sell Signal',
@@ -45,16 +43,87 @@ def plot_trading_strategy(market, portfolio, strategy, title='Trading Strategy O
         zorder=-1
     )
 
-    strategy.add_to_ax(axs[1])
+    min_y = market['close'].min()
+    max_y = market['close'].max()
+
+    # Shading holding periods
+    holding = False
+    for i in range(1, len(portfolio)):
+        if portfolio['positions'].iloc[i] == 1:  # Buy signal
+            start_date = portfolio.date[i]
+            holding = True
+        elif portfolio['positions'].iloc[i] == -1 and holding:  # Sell signal
+            end_date = portfolio.date[i]
+
+            ax.fill_betweenx(
+                y=[min_y, max_y],
+                x1=start_date,
+                x2=end_date,
+                color='gray',
+                alpha=0.3,
+            )
+            holding = False
 
     # Customize the main plot
-    axs[0].set_ylabel('Price')
-    axs[0].legend()
-    axs[0].grid(False)
+    ax.set_ylabel('Price')
+    ax.legend()
+    ax.grid(False)
 
-    # Customize the indicators plot
-    axs[1].legend()
-    axs[1].grid(True)
+
+def plot_assets(ax, market, portfolio, strategy):
+    ax.plot(
+        portfolio.date,
+        portfolio.total,
+        label='Total',
+        alpha=1,
+        linewidth=2,
+        zorder=-1
+    )
+
+    # ax.plot(
+    #     portfolio.date,
+    #     portfolio.holdings,
+    #     label='holdings',
+    #     alpha=1,
+    #     linewidth=2,
+    #     zorder=-1
+    # )
+
+    ax.legend()
+
+
+def plot_trading_strategy(market, portfolio, strategy, title='Trading Strategy Overview'):
+    """
+    Plots the trading strategy performance including price, buy/sell signals,
+    and other relevant indicators like moving averages or RSI.
+
+    Parameters:
+    - data: DataFrame containing the historical data and trading indicators/signals.
+    - title: str, optional. The title of the plot.
+    """
+    fig, axs = plt.subplots(3, 1, figsize=(7, 7), gridspec_kw={'height_ratios': [3, 1, 2]}, sharex=True)
+    fig.suptitle(title)
+
+    plot_price_and_signal(
+        ax=axs[0],
+        market=market,
+        portfolio=portfolio,
+        strategy=strategy
+    )
+
+    plot_assets(
+        ax=axs[2],
+        market=market,
+        portfolio=portfolio,
+        strategy=strategy
+    )
+
+    plot_metrics_on_ax(
+        ax=axs[1],
+        market=market,
+        portfolio=portfolio,
+        strategy=strategy
+    )
 
     # Format the x-axis
     for ax in axs:
--- plottings_examples.py
+++ plottings_examples.py
@@ -1,11 +1,11 @@
 from TradeTide.tools import get_dataframe
 from TradeTide.backtester import BackTester
-from TradeTide.strategy import MovingAverageCrossing, RelativeStrengthIndex, Random
+from TradeTide.strategy import RelativeStrengthIndex
 from TradeTide.plottings import plot_trading_strategy
 
 dataframe = get_dataframe('eur', 'usd', year=2020)
 
-market = dataframe[:100_000].copy()
+market = dataframe[:100_000]
 
 
 strategy = RelativeStrengthIndex(
@@ -13,8 +13,6 @@ strategy = RelativeStrengthIndex(
     oversold_threshold=30,
     overbought_threshold=70
 )
-
-# strategy = Random()
 
 strategy.generate_signal(market)
 
@@ -25,29 +23,15 @@ backtester = BackTester(
 )
 
 portfolio, metadata = backtester.back_test(
-    stop_loss=0.001,
-    take_profit=0.001,
-    # initial_capital=100_000,
+    stop_loss=0.01,
+    take_profit=0.01,
+    initial_capital=100_000,
     buy_unit=1_000,
     return_extra_data=True
 )
-
-# backtester.plot(
-#     # show_holdings=True,
-#     # show_take_win=True,
-#     # show_stop_loss=True,
-#     show_totals=True,
-#     # show_cash=True,
-# )
-
-
-
-
 
 plot_trading_strategy(
     market=market,
     portfolio=portfolio,
     strategy=strategy,
 )
-
-

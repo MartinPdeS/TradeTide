63e4b633b27aaa065dc77594ff488aafe9a08afb
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-08-13 11:58:28 -0400

    update

--- TradeTide/directories.py
+++ TradeTide/directories.py
@@ -9,6 +9,8 @@ root_path = Path(TradeTide.__path__[0])
 
 repository = project_path = Path(root_path)
 
+doc_css_path = project_path.parent / "docs/source/_static/default.css"
+
 data = repository / 'data'
 
 usd_eur = eur_usd = data.joinpath('eur_usd')

f9b876ae2516ffe177265c098de92329013779f0
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-12-23 19:04:30 +0100

    update

--- development/dev_0.py
+++ development/dev_0.py
@@ -1,19 +1,30 @@
-from TradeTide.binary import PortfolioInterface
+from TradeTide.binary import Portfolio, Position, Market, RiskManagement
+from datetime import datetime
 
+# Define start and end dates
+start_date = datetime(2023, 1, 1)
+end_date = datetime(2023, 12, 31)
 
-risk_manager = PortfolioInterface.RiskManagement(
-    initial_balance=100000, 
-    risk_per_trade=1.0, 
-    stop_loss=50
+risk_manager = RiskManagement(
+    initial_balance=100000,
+    risk_per_trade=1.0,
+    stop_loss=50,
+    take_profit=50
 )
 
-portfolio = PortfolioInterface.Portfolio(risk_manager=risk_manager)
 
-portfolio.add_position(
-    currency_pair="EUR/USD", 
-    is_long=True, 
-    entry_price=1.1000, 
-    pip_value=10.0
+market = Market(
+    start_date=start_date,
+    end_date=end_date
 )
 
-portfolio.print_metrics(risk_free_rate=2.0)
+
+market.display_market_data()
+
+# portfolio = Portfolio(risk_manager=risk_manager)
+
+# portfolio.process_signals(
+
+# )
+
+# portfolio.print_metrics(risk_free_rate=2.0)
